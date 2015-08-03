61bb175a377a0023a9ffdf4580a3e30baf4b4b9b
Will Daly <will@edx.org>
2015-08-03 13:44:34 -0700

    Require support role

--- lms/djangoapps/support/views.py
+++ lms/djangoapps/support/views.py
@@ -1,9 +1,27 @@
 """
 TODO -- views for student support
 """
+from functools import wraps
+
+from django.http import HttpResponseForbidden
+
 from edxmako.shortcuts import render_to_response
+from courseware.access import has_access
 
 
-def index(request):
+def require_support_permission(func):
+    """TODO """
+    @wraps(func)
+    def inner(request, *args, **kwargs):
+        if has_access(request.user, "support", "global"):
+            return func(request, *args, **kwargs)
+        else:
+            return HttpResponseForbidden()
+
+    return inner
+
+
+@require_support_permission
+def index(request, *args, **kwargs):
     """TODO """
     return render_to_response("support/index.html")
