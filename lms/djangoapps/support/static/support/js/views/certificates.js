d4cda66a8914ae987c805fe1ea4533a9e4b54b41
Will Daly <will@edx.org>
2015-08-04 12:54:38 -0700

    Work

---
+++ lms/djangoapps/support/static/support/js/views/certificates.js
@@ -0,0 +1,20 @@
+;(function (define) {
+    'use strict';
+
+    define(['backbone',
+            'underscore',
+            'gettext'],
+           function (Backbone, _, gettext) {
+               var view = Backbone.View.extend({
+                   initialize: function() {
+                      // TODO
+                   },
+
+                   render: function() {
+                      // TODO
+                   },
+               });
+
+               return view;
+           });
+}).call(this, define || RequireJS.define);
