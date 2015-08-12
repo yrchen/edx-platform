34a89a54258f4901932778a48115808c17d967a5
Will Daly <will@edx.org>
2015-08-12 09:45:16 -0700

    WIP

---
+++ openedx/core/djangoapps/credit/verification_access.py
@@ -0,0 +1,51 @@
+"""
+Apply in-course reverification access rules to a course.
+
+We model the rules as a set of user partitions, one for each
+verification checkpoint in a course.
+
+For example, suppose that a course has two verification checkpoints,
+one at midterm A and one at the midterm B.
+
+Then the user partitions would look like this:
+
+Midterm A:  |-- NON_VERIFIED --|-- VERIFIED_DENY --|-- VERIFIED_ALLOW --|
+Midterm B:  |-- NON_VERIFIED --|-- VERIFIED_DENY --|-- VERIFIED_ALLOW --|
+
+where the groups are defined as:
+
+* NON_VERIFIED: The user is not in a verified track, so the user sees exam
+   content, but not the verification checkpoint block.
+
+* VERIFIED_DENY: The user is in a verified track, but has not yet submitted
+   photos at the checkpoint.  The user can see the verification checkpoint block,
+   but not the exam content.
+
+* VERIFIED_ALLOW: The user is in a verfied track and has submitted photos
+   at the checkpoint.  The user can see both the verification checkpoint block
+   (which shows status messaging of the verification attempt) as well as the exam content.
+
+The code that puts users into particular groups based on their verification and enrollment
+status is a user partition scheme defined in the verify_student app.  This is just a function
+mapping users to the above groups for each verification-related partition in the course.
+
+The tricky thing is that we need to update the course structure to:
+(a) define partitions for each verification checkpoint
+(b) tag the allowed groups for each verification checkpoint block
+(c) tag the allowed groups for exam content associated with a verification checkpoint block.
+
+Once the content is tagged with the appropriate groups, the LMS can use
+the partition scheme function to put users into groups and check access.
+
+Ideally, this would be a transformation of the course structure run as a pre-processing
+step by the LMS.  Unfortunately, that infrastructure does not yet exist, so currently we're
+modifying the course automatically on publish from Studio.
+
+"""
+
+
+def apply_verification_access_rules(course_key):
+    """
+    TODO
+    """
+    pass
