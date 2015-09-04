4b2d5594f50448bc146cadb83b70ce874da5af27
David Baumgold <david@davidbaumgold.com>
2015-09-04 14:48:26 -0400

    Fetch people.yaml file from private repo

--- scripts/release.py
+++ scripts/release.py
@@ -36,7 +36,7 @@ PROJECT_ROOT = path(__file__).abspath().dirname()
 repo = Repo(PROJECT_ROOT)
 git = repo.git
 
-PEOPLE_YAML = "https://raw.githubusercontent.com/edx/repo-tools/master/people.yaml"
+PEOPLE_YAML = "https://raw.githubusercontent.com/edx/repo-tools-data/master/people.yaml"
 
 
 class memoized(object):
@@ -140,7 +140,10 @@ def create_github_creds():
     https://developer.github.com/v3/oauth_authorizations/#create-a-new-authorization
     """
     headers = {"User-Agent": "edx-release"}
-    payload = {"note": "edx-release"}
+    payload = {
+        "note": "edx-release",
+        "scopes": ["repo"],
+    }
     username = raw_input("Github username: ")
     password = getpass.getpass("Github password: ")
     response = requests.post(
@@ -291,6 +294,7 @@ class DoesNotExist(Exception):
         self.message = message
         self.commit = commit
         self.branch = branch
+        Exception.__init__(self, message)
 
 
 def get_merge_commit(commit, branch="master"):
@@ -362,10 +366,15 @@ def prs_by_email(start_ref, end_ref):
     The dictionary is alphabetically ordered by email address
     The pull request list is ordered by merge date
     """
+    username, token = get_github_creds()
+    headers = {
+        "Authorization": "token {}".format(token),
+        "User-Agent": "edx-release",
+    }
     # `emails` maps from other_emails to primary email, based on people.yaml.
     emails = {}
     try:
-        people_resp = requests.get(PEOPLE_YAML)
+        people_resp = requests.get(PEOPLE_YAML, headers=headers)
         people_resp.raise_for_status()
         people = yaml.safe_load(people_resp.text)
     except requests.exceptions.RequestException as e:
